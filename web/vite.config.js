b93aeae28cd525c0385787dbefbe4c36848302d4
Yuri Nicolau <ncl.yuri@gmail.com>
2024-06-23 02:58:58 -0300

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,25 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  clearScreen: true,
+  build: {
+    rollupOptions: {
+      output: {
+        // Configure how the output files are named:
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: ({ name }) => {
+          // Handle assets based on their folder structure and extension
+          if (name.includes('src/assets')) {
+            // Put files originally in src/assets into dist/assets
+            return 'assets/[name].[ext]'
+          }
+          // Other files go into a general assets folder
+          return 'assets/[name].[ext]'
+        },
+      },
+    },
+  },
 })
