217e4c725edf1eab2f1f3a9ce22c0b49f5bf53ce
Yuri Nicolau <ncl.yuri@gmail.com>
2024-06-23 02:19:20 -0300

    final changes for deploy

--- api/src/server.js
+++ api/src/server.js
@@ -10,10 +10,16 @@ const uploadConfig = require('./configs/upload')
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173/'],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      'https://yurinicolauu-food-explorer.netlify.app/',
+      'http://yurinicolauu-food-explorer.netlify.app/',
+      'http://127.0.0.1:5173/',
+    ],
+    credentials: true,
+  })
+)
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 app.use(cookieParser())
